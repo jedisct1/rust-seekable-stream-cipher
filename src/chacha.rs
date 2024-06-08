dc431bd64291fbf457cae464acf8706410f5bc0e
Frank Denis <github@pureftpd.org>
2024-06-08 16:12:56 +0200

    Use regular ChaCha instead of HChaCha

--- src/chacha.rs
+++ src/chacha.rs
@@ -34,10 +34,10 @@ impl StreamCipher {
             u32::from_le_bytes(key[20..24].try_into().unwrap()),
             u32::from_le_bytes(key[24..28].try_into().unwrap()),
             u32::from_le_bytes(key[28..32].try_into().unwrap()),
+            0,
+            0,
             u32::from_le_bytes(id[0..4].try_into().unwrap()),
             u32::from_le_bytes(id[4..8].try_into().unwrap()),
-            0,
-            0,
         ];
         StreamCipher { st }
     }
@@ -45,25 +45,21 @@ impl StreamCipher {
     /// Squeeze a 32-byte block, and store it in the given buffer.
     #[inline(always)]
     fn store_rate(mut self, out: &mut [u8], block_offset: u64) {
-        self.st[14] = block_offset as _;
-        self.st[15] = (block_offset >> 32) as _;
+        self.st[12] = block_offset as _;
+        self.st[13] = (block_offset >> 32) as _;
         self.permute();
-        out[0 * 4..][0..4].copy_from_slice(&(self.st[0]).to_le_bytes());
-        out[1 * 4..][0..4].copy_from_slice(&(self.st[1]).to_le_bytes());
-        out[2 * 4..][0..4].copy_from_slice(&(self.st[2]).to_le_bytes());
-        out[3 * 4..][0..4].copy_from_slice(&(self.st[3]).to_le_bytes());
-        out[4 * 4..][0..4].copy_from_slice(&(self.st[12]).to_le_bytes());
-        out[5 * 4..][0..4].copy_from_slice(&(self.st[13]).to_le_bytes());
-        out[6 * 4..][0..4].copy_from_slice(&(self.st[14]).to_le_bytes());
-        out[7 * 4..][0..4].copy_from_slice(&(self.st[15]).to_le_bytes());
+        for i in 0..16 {
+            out[i * 4..][0..4].copy_from_slice(&(self.st[i]).to_le_bytes());
+        }
     }
 
     /// Squeeze a 32-byte block, and add it to the given buffer.
     #[inline(always)]
     fn apply_rate(mut self, out: &mut [u8], block_offset: u64) {
-        self.st[14] = block_offset as _;
-        self.st[15] = (block_offset >> 32) as _;
+        self.st[12] = block_offset as _;
+        self.st[13] = (block_offset >> 32) as _;
         self.permute();
+
         let out0 = u32::from_le_bytes(out[0 * 4..][0..4].try_into().unwrap());
         let out1 = u32::from_le_bytes(out[1 * 4..][0..4].try_into().unwrap());
         let out2 = u32::from_le_bytes(out[2 * 4..][0..4].try_into().unwrap());
@@ -72,20 +68,36 @@ impl StreamCipher {
         let out5 = u32::from_le_bytes(out[5 * 4..][0..4].try_into().unwrap());
         let out6 = u32::from_le_bytes(out[6 * 4..][0..4].try_into().unwrap());
         let out7 = u32::from_le_bytes(out[7 * 4..][0..4].try_into().unwrap());
+        let out8 = u32::from_le_bytes(out[8 * 4..][0..4].try_into().unwrap());
+        let out9 = u32::from_le_bytes(out[9 * 4..][0..4].try_into().unwrap());
+        let out10 = u32::from_le_bytes(out[10 * 4..][0..4].try_into().unwrap());
+        let out11 = u32::from_le_bytes(out[11 * 4..][0..4].try_into().unwrap());
+        let out12 = u32::from_le_bytes(out[12 * 4..][0..4].try_into().unwrap());
+        let out13 = u32::from_le_bytes(out[13 * 4..][0..4].try_into().unwrap());
+        let out14 = u32::from_le_bytes(out[14 * 4..][0..4].try_into().unwrap());
+        let out15 = u32::from_le_bytes(out[15 * 4..][0..4].try_into().unwrap());
         out[0 * 4..][0..4].copy_from_slice(&(out0 ^ self.st[0]).to_le_bytes());
         out[1 * 4..][0..4].copy_from_slice(&(out1 ^ self.st[1]).to_le_bytes());
         out[2 * 4..][0..4].copy_from_slice(&(out2 ^ self.st[2]).to_le_bytes());
         out[3 * 4..][0..4].copy_from_slice(&(out3 ^ self.st[3]).to_le_bytes());
-        out[4 * 4..][0..4].copy_from_slice(&(out4 ^ self.st[12]).to_le_bytes());
-        out[5 * 4..][0..4].copy_from_slice(&(out5 ^ self.st[13]).to_le_bytes());
-        out[6 * 4..][0..4].copy_from_slice(&(out6 ^ self.st[14]).to_le_bytes());
-        out[7 * 4..][0..4].copy_from_slice(&(out7 ^ self.st[15]).to_le_bytes());
+        out[4 * 4..][0..4].copy_from_slice(&(out4 ^ self.st[4]).to_le_bytes());
+        out[5 * 4..][0..4].copy_from_slice(&(out5 ^ self.st[5]).to_le_bytes());
+        out[6 * 4..][0..4].copy_from_slice(&(out6 ^ self.st[6]).to_le_bytes());
+        out[7 * 4..][0..4].copy_from_slice(&(out7 ^ self.st[7]).to_le_bytes());
+        out[8 * 4..][0..4].copy_from_slice(&(out8 ^ self.st[8]).to_le_bytes());
+        out[9 * 4..][0..4].copy_from_slice(&(out9 ^ self.st[9]).to_le_bytes());
+        out[10 * 4..][0..4].copy_from_slice(&(out10 ^ self.st[10]).to_le_bytes());
+        out[11 * 4..][0..4].copy_from_slice(&(out11 ^ self.st[11]).to_le_bytes());
+        out[12 * 4..][0..4].copy_from_slice(&(out12 ^ self.st[12]).to_le_bytes());
+        out[13 * 4..][0..4].copy_from_slice(&(out13 ^ self.st[13]).to_le_bytes());
+        out[14 * 4..][0..4].copy_from_slice(&(out14 ^ self.st[14]).to_le_bytes());
+        out[15 * 4..][0..4].copy_from_slice(&(out15 ^ self.st[15]).to_le_bytes());
     }
 
-    /// Squeeze and return a 32-byte block.
+    /// Squeeze and return a 64-byte block.
     #[inline(always)]
-    fn squeeze_rate(self, block_offset: u64) -> [u8; 32] {
-        let mut out = [0u8; 32];
+    fn squeeze_rate(self, block_offset: u64) -> [u8; 64] {
+        let mut out = [0u8; 64];
         self.store_rate(&mut out, block_offset);
         out
     }
@@ -99,18 +111,18 @@ impl StreamCipher {
         if start_offset.checked_add(out.len() as u64).is_none() {
             return Err("offset would overflow");
         }
-        let mut block_offset = start_offset / 32;
-        let offset_in_first_block = (start_offset % 32) as usize;
-        let bytes_to_copy = cmp::min(32 - offset_in_first_block, out.len());
+        let mut block_offset = start_offset / 64;
+        let offset_in_first_block = (start_offset % 64) as usize;
+        let bytes_to_copy = cmp::min(64 - offset_in_first_block, out.len());
         if bytes_to_copy > 0 {
             let rate = self.squeeze_rate(block_offset);
             out[..bytes_to_copy].copy_from_slice(&rate[offset_in_first_block..][..bytes_to_copy]);
             out = &mut out[bytes_to_copy..];
         }
-        while out.len() >= 32 {
+        while out.len() >= 64 {
             block_offset += 1;
-            self.store_rate(&mut out[..32], block_offset);
-            out = &mut out[32..];
+            self.store_rate(&mut out[..64], block_offset);
+            out = &mut out[64..];
         }
         if !out.is_empty() {
             block_offset += 1;
@@ -140,9 +152,9 @@ impl StreamCipher {
         if start_offset.checked_add(out.len() as u64).is_none() {
             return Err("offset would overflow");
         }
-        let mut block_offset = start_offset / 32;
-        let offset_in_first_block = (start_offset % 32) as usize;
-        let bytes_to_copy = cmp::min(32 - offset_in_first_block, out.len());
+        let mut block_offset = start_offset / 64;
+        let offset_in_first_block = (start_offset % 64) as usize;
+        let bytes_to_copy = cmp::min(64 - offset_in_first_block, out.len());
         if bytes_to_copy > 0 {
             let rate = self.squeeze_rate(block_offset);
             for i in 0..bytes_to_copy {
@@ -150,10 +162,10 @@ impl StreamCipher {
             }
             out = &mut out[bytes_to_copy..];
         }
-        while out.len() >= 32 {
+        while out.len() >= 64 {
             block_offset += 1;
-            self.apply_rate(&mut out[..32], block_offset);
-            out = &mut out[32..];
+            self.apply_rate(&mut out[..64], block_offset);
+            out = &mut out[64..];
         }
         if !out.is_empty() {
             block_offset += 1;
@@ -166,6 +178,7 @@ impl StreamCipher {
     }
 
     fn permute(&mut self) {
+        let mask = self.st;
         let x = &mut self.st;
         for _ in 0..12 / 2 {
             {
@@ -256,6 +269,9 @@ impl StreamCipher {
                 x[R[2]] = x[R[2]].wrapping_add(x[R[3]]);
                 x[R[1]] = (x[R[1]] ^ x[R[2]]).rotate_left(7);
             }
+        }
+        for i in 0..16 {
+            x[i] = x[i].wrapping_add(mask[i]);
         }
     }
 }
